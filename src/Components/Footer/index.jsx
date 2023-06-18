9f863b55f761ba71bb6b86e7c48bb01b53bff387
mirandalu2020 <yuanling1007@hotmail.com>
2023-06-18 08:09:39 -0700

    readme updated with uml

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.scss';
+
+function Footer(props){
+  return <footer>&copy; 2018</footer>;
+}
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+export default Footer;
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.scss';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>RESTy</h1>
+    </header>
+  )
+}
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>RESTy</h1>
+//       </header>
+//     );
+//   }
+// }
+
+export default Header;
